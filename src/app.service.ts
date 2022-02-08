d051a71341b4272879cd264c6c133e8064945c91
고 성진 <rtw456@jinhak.com>
2022-02-08 09:58:26 +0900

    describe service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+// 비즈니스 로직을 실행하는 역할
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello Nest';
+  }
+  getHi(): string {
+    return 'Hi Nest';
   }
 }
