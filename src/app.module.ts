4b61669f3ccfbb66829caba54f72f91dc0d9ed4f
고 성진 <rtw456@jinhak.com>
2022-02-08 09:38:43 +0900

    add decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// @ -> 데코레이터: 클래스에 함수 기능 추가
 @Module({
   imports: [],
   controllers: [AppController],
