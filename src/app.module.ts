0330aaf380ce0df69911d8fddf47d37b808bb736
고 성진 <rtw456@jinhak.com>
2022-02-08 10:20:22 +0900

    add controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 // @ -> 데코레이터: 클래스에 함수 기능 추가
 @Module({
   imports: [],
-  controllers: [AppController], // url을 가져오고 함수를 실행, express의 라우터 기능
+  controllers: [AppController, MoviesController], // url을 가져오고 함수를 실행, express의 라우터 기능
   providers: [AppService],
 })
 export class AppModule {}
