783ab35efeb8fa57a35cb346e3396b49d035fed0
고 성진 <rtw456@jinhak.com>
2022-02-08 11:21:50 +0900

    add service logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 // @ -> 데코레이터: 클래스에 함수 기능 추가
 @Module({
   imports: [],
   controllers: [AppController, MoviesController], // url을 가져오고 함수를 실행, express의 라우터 기능
-  providers: [AppService],
+  providers: [AppService, MoviesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService){}
+
     @Get()
-    getAll() {
-        return 'This will return all movies';
+    getAll() : Movie[]{
+        return this.moviesService.getAll();
     }
 
     @Get("search")
@@ -14,18 +18,18 @@ export class MoviesController {
     }
 
     @Get("/:id")
-    getOne(@Param("id") movieId: string) {
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param("id") movieId: string): Movie {
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
     remove(@Param('id') movieId: string) {
-        return `This will delete a movie with the id ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
