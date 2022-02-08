8c5bd06192f81a8af8a5d801ccb2761f7aa7d759
고 성진 <rtw456@jinhak.com>
2022-02-08 09:52:56 +0900

    describe controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 // @ -> 데코레이터: 클래스에 함수 기능 추가
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // url을 가져오고 함수를 실행, express의 라우터 기능
   providers: [AppService],
 })
 export class AppModule {}
